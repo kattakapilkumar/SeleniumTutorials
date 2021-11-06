37a80a19a136819e85790731f1af7a50f42c750d
kattakak <kapilkatta123@gmail.com>
2021-11-06 16:03:50 +0530

    ExtentReports

--- src/test/java/Utility/ExtentRprts.java
+++ src/test/java/Utility/ExtentRprts.java
@@ -1,11 +1,18 @@
 package Utility;
 
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.BeforeTest;
+
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
 import com.aventstack.extentreports.reporter.ExtentReporter;
 import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 
 public class ExtentRprts {
+	
+	
+	@BeforeSuite
+	@BeforeTest
 	public void SetupExtents() {
 		ExtentSparkReporter spark = new ExtentSparkReporter("Spark.html");
 		ExtentReports extent = new ExtentReports();
