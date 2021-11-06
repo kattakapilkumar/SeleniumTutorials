2eea8fc26116f81887e53ab332581fc3ba1bbae3
kattakak <kapilkatta123@gmail.com>
2021-11-06 20:31:40 +0530

    ExtentReports

--- src/test/java/StepDefinations/LoginDemoStepdefination2_PF.java
+++ src/test/java/StepDefinations/LoginDemoStepdefination2_PF.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 import Utility.BaseSetupclass;
@@ -17,11 +18,12 @@ import pagefactory.LoginPage_PF;
 
 public class LoginDemoStepdefination2_PF extends BaseSetupclass {
 	
+
 	WebDriver driver = null;
 	
 	LoginPage_PF obj ;
 	
-	
+	@Test (priority=1)
 	@Given("brower is open")
 	public void brower_is_open() throws InterruptedException {
 		
@@ -39,15 +41,16 @@ public class LoginDemoStepdefination2_PF extends BaseSetupclass {
 	    Thread.sleep(2000);
 	}
 
-
+	@Test (priority=2)
 	@Given("user is on login page")
 	public void user_is_on_login_page() {
 		
 		driver.navigate().to("https://example.testproject.io/web/");
 		System.out.println("i am here");
 	}
-
-	
+    
+	@Test (priority=3)
+	@Parameters({"username","password"})
 	@When("^user enters (.*) and (.*)$")
 	public void user_enters_username_and_password(String username,String password) throws InterruptedException {
 		
@@ -66,7 +69,7 @@ public class LoginDemoStepdefination2_PF extends BaseSetupclass {
 
 	}
 
-
+	@Test (priority=4)
 	@Then("user is navigated")
 	public void user_is_navigated() {
 		
